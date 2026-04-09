c9ad94303761eefefb75cb1500cf6e1dffac6c35
SAATHVIKHOSUR121 <saathvikhosur@gmail.com>
2026-04-09 18:57:39 +0200

    Add files via upload

---
+++ satalite game/theconnecter.py
@@ -0,0 +1,49 @@
+import random, pgzrun, time
+WIDTH = 800
+HEIGHT = 500
+#variables for the game
+satalitelis = []
+lineslis = []
+totalsatalites = 25
+startime = 0
+totalime = 0
+endtime = 0
+nextsatalitecounter = 0
+# Function for satalites
+def satafun():
+    global startime
+    for i in range(totalsatalites):
+        satalite = Actor("thesatalite.png")
+        satalite.x = random.randint(50,750)
+        satalite.y = random.randint(50,450)
+        satalitelis.append(satalite)
+    startime = time.time()
+def draw():
+    global totaltime
+    screen.blit("spacebg.jpg", (0,0))
+    sequence = 1
+    for v in satalitelis:
+        v.draw()
+        screen.draw.text(str(sequence),(v.pos[0], v.pos[1]+20),color = "Yellow") 
+        sequence = sequence + 1
+    for i in lineslis:
+        screen.draw.line(i[0], i[1], color = "white" )
+    if nextsatalitecounter < totalsatalites:
+        totaltime = time.time() - startime  
+        screen.draw.text(str(round(totaltime,1)),(10,10), color = "black")
+
+# function for mouse click
+def on_mouse_down(pos):
+    global nextsatalitecounter, lineslis
+    if nextsatalitecounter < totalsatalites:
+        if satalitelis[nextsatalitecounter].collidepoint(pos):
+            if nextsatalitecounter:
+                lineslis.append((satalitelis[nextsatalitecounter-1].pos, satalitelis[nextsatalitecounter-1].pos))
+            nextsatalitecounter = nextsatalitecounter+1
+
+
+
+def update():
+    pass
+satafun()
+pgzrun.go()
