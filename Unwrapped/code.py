5e371ec401746eccef3ffebdd5c580c939197519
SAATHVIKHOSUR121 <saathvikhosur@gmail.com>
2026-05-21 18:57:06 +0200

    Add files via upload

--- Unwrapped/code.py
+++ Unwrapped/code.py
@@ -2,17 +2,37 @@ import random, pgzrun
 WIDTH = 800
 HEIGHT = 450
 x = 0
+y = 0
 gummy = Actor("gummy.png")
-
+sodacan = Actor("itemsodacan")
+bottle = Actor("itembottle")
+tomato = Actor("itemtomato")
+head = Actor("pixalalatedhead")
 def draw():
     screen.clear()
     screen.blit("firstbg", (x,0))
     screen.blit("secondbg", (x + 800,0))
+
+    tomato.draw()
+    tomato.pos = (x + 280,330)
+    sodacan.draw()
+    sodacan.pos = (x + 180,290) 
+    bottle.draw()
+    bottle.pos = (x + 440,130)
+    head.draw()
+    head.pos = (-160,200)
+
+
     gummy.draw()
 def update():
     global x
+    x-=0.5
     if keyboard.right: 
-        x-= 1
+        gummy.x += 2
     if keyboard.left:
-        x+= 1
+        gummy.x -= 2
+    if keyboard.up:
+        gummy.y -= 2
+    if keyboard.down:
+        gummy.y += 2
 pgzrun.go()
