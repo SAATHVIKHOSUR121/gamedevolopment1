3e8ce50287bf385f4537a790b3b470086555aa6b
SAATHVIKHOSUR121 <saathvikhosur@gmail.com>
2026-06-04 18:58:57 +0200

    Add files via upload

--- Unwrapped/code.py
+++ Unwrapped/code.py
@@ -3,29 +3,43 @@ WIDTH = 800
 HEIGHT = 450
 x = 0
 y = 0
+screenx = 400
+screen1 = Actor("firstbg", (screenx,225))
+screen2 = Actor("secondbg", (screenx + 800,225))
 gummy = Actor("gummy.png")
 sodacan = Actor("itemsodacan")
 bottle = Actor("itembottle")
 tomato = Actor("itemtomato")
 head = Actor("pixalalatedhead")
+itemslist = [sodacan, bottle, tomato]
+score = 0
+gameover = False
+poslist = [[180,290], [440,130], [280,330]]
 def draw():
+    if gameover == True:
+        screen.blit("lose.jpg", (0,0))
+        return
     screen.clear()
-    screen.blit("firstbg", (x,0))
-    screen.blit("secondbg", (x + 800,0))
+    screen1.draw()
+    screen1.pos = (screenx,225)
+    screen2.draw()
+    screen2.pos = (screenx + 800,225)
 
-    tomato.draw()
-    tomato.pos = (x + 280,330)
-    sodacan.draw()
-    sodacan.pos = (x + 180,290) 
-    bottle.draw()
-    bottle.pos = (x + 440,130)
+    for i in range(len(itemslist)):
+        itemslist[i].pos = (x + poslist[i][0],poslist[i][1])
+        itemslist[i].draw()
+    
+    
     head.draw()
-    head.pos = (-160,200)
+    head.pos = (-150,200)
 
 
     gummy.draw()
+    
 def update():
-    global x
+    
+    global x, score, gameover, screenx
+    screenx-=0.5
     x-=0.5
     if keyboard.right: 
         gummy.x += 2
@@ -35,4 +49,11 @@ def update():
         gummy.y -= 2
     if keyboard.down:
         gummy.y += 2
+    for item in itemslist:
+        if gummy.colliderect(item):
+            score = score + 1 
+    if gummy.colliderect(screen2) and score != 3:
+        gameover = True
+
+        
 pgzrun.go()
