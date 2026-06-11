29956d2298d07f8a522affdd2ac04b92f0ea6561
SAATHVIKHOSUR121 <saathvikhosur@gmail.com>
2026-06-11 18:54:56 +0200

    Add files via upload

--- Unwrapped/code.py
+++ Unwrapped/code.py
@@ -14,7 +14,8 @@ head = Actor("pixalalatedhead")
 itemslist = [sodacan, bottle, tomato]
 score = 0
 gameover = False
-poslist = [[180,290], [440,130], [280,330]]
+#poslist = [[180,290], [440,130], [280,330]]
+poslist = [[100,290], [100, 130], [100, 350]]
 def draw():
     if gameover == True:
         screen.blit("lose.jpg", (0,0))
@@ -24,14 +25,13 @@ def draw():
     screen1.pos = (screenx,225)
     screen2.draw()
     screen2.pos = (screenx + 800,225)
-
-    for i in range(len(itemslist)):
-        itemslist[i].pos = (x + poslist[i][0],poslist[i][1])
-        itemslist[i].draw()
-    
-    
     head.draw()
-    head.pos = (-150,200)
+    head.pos = (-150,200) 
+    print(len(itemslist))
+    for i in range(len(itemslist)):
+        itemslist[i].pos = (screenx + poslist[i][0],poslist[i][1])
+        itemslist[i].draw()
+
 
 
     gummy.draw()
@@ -39,8 +39,8 @@ def draw():
 def update():
     
     global x, score, gameover, screenx
-    screenx-=0.5
-    x-=0.5
+    #screenx-=0.5
+    #x-=0.5
     if keyboard.right: 
         gummy.x += 2
     if keyboard.left:
@@ -50,10 +50,14 @@ def update():
     if keyboard.down:
         gummy.y += 2
     for item in itemslist:
+       
         if gummy.colliderect(item):
             score = score + 1 
+            print(score)
+            itemslist.remove(item)
     if gummy.colliderect(screen2) and score != 3:
         gameover = True
+
 
         
 pgzrun.go()
